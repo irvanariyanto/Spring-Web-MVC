b3dcceba24a192a928b1d43792a2c5e2aa1c5626
Irvan Ariyanto <irvan.ariyanto104@gmail.com>
2017-05-11 12:43:59 +0700

    Spring boot JPA

---
+++ src/main/java/com/springweb/model/Mahasiswa.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.springweb.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+
+/**
+ *
+ * @author BAY
+ */
+@Entity
+//@Table(name="")
+public class Mahasiswa {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    
+    @Column(name = "nama_mahasiswa")
+    private String nama;
+    
+    private String jurusan;
+    private String nim;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getJurusan() {
+        return jurusan;
+    }
+
+    public void setJurusan(String jurusan) {
+        this.jurusan = jurusan;
+    }
+
+    public String getNim() {
+        return nim;
+    }
+
+    public void setNim(String nim) {
+        this.nim = nim;
+    }
+    
+    
+}
